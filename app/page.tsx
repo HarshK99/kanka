0c27a7cf17e26b2cf3c543316cd3b2efa8325982
HarshK <harshkankaria9@gmail.com>
2026-01-14 17:35:23 +0530

    bg color fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero, About, Projects, Writing, ClientProjects, Contact, Footer } from 
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white pt-24">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black shadow-2xl text-white pt-24">
       <Hero />
       <About />
       <Projects />
